a541f2611e040e795cf185f46459c3c686dd66b0
ngtimer <gomesnikas@gmail.com>
2025-05-15 11:41:11 +0200

    Update stg_sales.sql

--- models/staging /stg_sales.sql
+++ models/staging /stg_sales.sql
@@ -1,1 +1,16 @@
+with raw as (
+    select
+        cast("InvoiceNo" as string) as invoice_no,
+        cast("StockCode" as string) as stock_code,
+        cast("Description" as string) as description,
+        cast("Quantity" as integer) as quantity,
+        cast("InvoiceDate" as timestamp) as invoice_date,
+        cast("UnitPrice" as float) as unit_price,
+        cast("CustomerID" as string) as customer_id,
+        cast("Country" as string) as country
+    from {{ source('sales', 'sales') }}
+)
+
+select * from raw
+where customer_id is not null and unit_price is not null
 
